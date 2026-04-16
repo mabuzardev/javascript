4172a2c3a0d392c7670fbadca5f31db969d0ca12
Mohammad Abuzar <mohammadabuzar316@gmail.com>
2026-04-16 10:43:35 +0530

    day 1 problem solving

---
+++ 21-days-of-problem-solving/Day-1_simple-number-problems/script.js
@@ -0,0 +1,31 @@
+console.log("Hello, World!");
+
+// const num = 20;
+
+// for (let i = 1; i <= num; i++) {
+//   console.log(num - i);
+// }
+
+// const num = 100;
+
+// for (let i = 1; i <= num; i++) {
+//   if (i % 2 === 0) {
+//     console.log("even", i);
+//   }
+// }
+
+// const num = 10;
+// let sum = 0;
+
+// for (let i = 1; i <= num; i++) {
+//   sum += i;
+// }
+
+// console.log("sum", sum);
+
+// const num = 10;
+// let sum = (num * (num + 1)) / 2;
+
+// console.log("sum", sum);
+
+
