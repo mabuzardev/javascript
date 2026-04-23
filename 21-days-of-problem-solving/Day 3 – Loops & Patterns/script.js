31c60491ff23a40f9fbbb7f4feedfe94b35b7a47
Mohammad Abuzar <mohammadabuzar316@gmail.com>
2026-04-23 11:04:42 +0530

    day 3

--- 21-days-of-problem-solving/Day 3 – Loops & Patterns/script.js
+++ 21-days-of-problem-solving/Day 3 – Loops & Patterns/script.js
@@ -13,4 +13,26 @@ const RightENgledStarTriangle = (row) => {
   console.log(str);
 };
 
-RightENgledStarTriangle(6);
+// RightENgledStarTriangle(6);
+
+// Print Pyramid Pattern
+
+const pyramidPattern = (row) => {
+  let str = "";
+
+  for (let i = 1; i <= 2 * row - 1; i += 2) {
+    let currentRow = (i + 1) / 2;
+
+    for (let j = 1; j <= row + currentRow - 1; j++) {
+      if (j <= row - currentRow) {
+        str += " ";
+      } else {
+        str += "*";
+      }
+    }
+
+    str += "\n";
+  }
+  console.log(str);
+};
+pyramidPattern(5);
